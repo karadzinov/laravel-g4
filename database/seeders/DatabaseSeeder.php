3cf27bd5f2eba970ca55ee16db448a0e590d1891
martin <martin@pingdevs.com>
2023-02-06 19:26:44 +0100

    Creating Countries table and DB Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,6 @@ class DatabaseSeeder extends Seeder
         // ]);
        // $this->call(RoleSeeder::class);
        // $this->call(UserSeeder::class);
-
+        $this->call(CountriesSeeder::class);
     }
 }
