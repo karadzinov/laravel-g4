74cc75f27c2c63a133186e2b0d8bb5e39525637a
martin <martin@pingdevs.com>
2023-02-08 21:21:50 +0100

    Creating API with Passport and Swagger Documentation

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->group(function()  {
-    Route::get('user', [App\Http\Controllers\Api\UserController::class, 'index']);
-    Route::get('users', [App\Http\Controllers\Api\UserController::class, 'all']);
+Route::middleware('auth:api')->group(function() {
+    Route::get('/user', [\App\Http\Controllers\Api\UserController::class, 'index']);
+    Route::get('/users', [\App\Http\Controllers\Api\UserController::class, 'all']);
+    Route::get('/user/{user}/edit', [\App\Http\Controllers\Api\UserController::class, 'edit']);
+    Route::put('/user/{user}',  [\App\Http\Controllers\Api\UserController::class, 'update']);
+    Route::post('/users', [\App\Http\Controllers\Api\UserController::class, 'store']);
+    Route::delete('/users/{user}', [\App\Http\Controllers\Api\UserController::class, 'destroy']);
 });
 
